90d4e275370ce8847048d88cc716aac9e1b3dec0
Saksham Aggarwal <saksham28jan@gmail.com>
2025-12-09 04:30:57 +0530

    update

--- Step 2 - Learn Important Sorting Techniques/Lec 2- Sorting-II/MergeSort.cpp
+++ Step 2 - Learn Important Sorting Techniques/Lec 2- Sorting-II/MergeSort.cpp
@@ -1,34 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> mergeSort(vector<int>& nums) {
-        mergeSortHelper(nums, 0, nums.size());
+    vector<int> mergeSort(vector<int> nums) {
+        mergeSortHelper(nums, 0, nums.size()-1);
+        return nums;
     }
-    vector<int> mergeSortHelper(vector<int>& nums, int low, int high) {
-        if (low >= high) return nums;
+    void mergeSortHelper(vector<int>& nums, int low, int high){
+        if (low >= high) return;
 
-        int mid = (low + high)/2 ;
+        int mid = (low + high) / 2;
 
         mergeSortHelper(nums, low, mid);
-        mergeSortHelper(nums, mid+1, high);
+        mergeSortHelper(nums, mid + 1, high);
 
         merge(nums, low, mid, high);
     }
-    vector<int> merge(vector<int>& nums, int low, int mid, int high){
-        vector<int> result;
-
-        for (int i = low; i < mid; i++){
-    
-            if (nums[mid + 1] > nums[i]){
-                result.pushback(nums[])
-                for (int j = mid + 1; j <= high; j++){
-                    
-                }
-                result.pushback(nums[j]);
+    void merge(vector<int>& nums, int low, int mid, int high){
+        vector<int> temp;
+        int left = low, right = mid + 1;
+        
+        while (left <= mid && right <= high){
+            if (nums[left] > nums[right]){
+                temp.push_back(nums[right++]);
+            } else {
+                temp.push_back(nums[left++]);
             }
-            else {
-                result.pushback(nums[i]);
-            }
-            
+        }
+        while (left <= mid){
+            temp.push_back(nums[left++]);
+        }
+        while (right <= high){
+            temp.push_back(nums[right++]);
+        }
+        for (int i = low; i <= high; i++){
+            nums[i] = temp[i - low];
         }
     }
 };
+
+int main(){
+    Solution sol;
+    vector<int> result = sol.mergeSort({7,4,6,1,8});
+    for(int num : result){
+        cout << num << " ";
+    }
+    return 0;
+}

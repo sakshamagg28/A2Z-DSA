f4ade1c9fafc7f2aa1e9689b9a5ff1d49745597d
Saksham Aggarwal <saksham28jan@gmail.com>
2025-12-08 05:48:59 +0530

    update

---
+++ Step 2 : Learn Important Sorting Techniques/Lec 1: Sorting-I/SelectionSort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> selectionSort(vector<int> nums) {  
+        for (int i = 0; i < nums.size(); i++){
+            for (int j = i; j < nums.size(); j++){
+                if (nums[j] < nums[i]) swap(nums[i], nums[j]);
+            }
+        }
+        return nums;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> v = sol.selectionSort({7,23,6,7,1,6,78}); 
+    for (auto itr = v.begin(); itr != v.end(); itr++){
+        cout << *itr << " ";
+    }
+    return 0;
+}

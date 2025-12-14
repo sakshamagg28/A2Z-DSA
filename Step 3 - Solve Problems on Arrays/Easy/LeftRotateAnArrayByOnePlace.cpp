4370db836d0d25b6bad509c115a47a34e7f5c0b5
Saksham Aggarwal <saksham28jan@gmail.com>
2025-12-14 23:51:26 +0530

    update

---
+++ Step 3 - Solve Problems on Arrays/Easy/LeftRotateAnArrayByOnePlace.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int temp = nums[0];
+        for (int i = 0; i < nums.size()-1; i++) {
+            nums[i] = nums[i+1];
+        }
+        nums[nums.size()-1] = temp;
+    }
+};

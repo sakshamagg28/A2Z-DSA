e573df2e5a22c614df34384538861173e09e4f10
Saksham Aggarwal <saksham28jan@gmail.com>
2025-12-14 21:28:35 +0530

    update

--- Step 2 - Learn Important Sorting Techniques/Lec 2- Sorting-II/QuickSort.cpp
+++ Step 2 - Learn Important Sorting Techniques/Lec 2- Sorting-II/QuickSort.cpp
@@ -5,7 +5,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> quickSort(vector<int> nums) {
-
+        quickSortHelper(nums, 0, nums.size() - 1);
+        return nums;
     }
     void quickSortHelper(vector<int>& nums, int low, int high){
         if (low >= high) return;
@@ -16,7 +17,17 @@ public:
         quickSortHelper(nums, pivotIndex + 1, high);
     }
     int partition(vector<int>& nums, int low, int high){
-        int pivotIndex 
+        int pivotElement = nums[high];
+        int i = low - 1;
+        
+        for(int j = low; j < high; j++){
+            if (nums[j] < pivotElement) {
+                i++;
+                swap(nums[j], nums[i]);
+            }
+        }
+        swap(nums[i + 1], nums[high]);
+        return i + 1;
     }
 };
 

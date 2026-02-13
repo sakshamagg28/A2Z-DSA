4d1cb4df4aac11f501fed0b18699dd8b7e56aab9
Saksham Aggarwal <saksham28jan@gmail.com>
2026-02-13 18:33:08 +0530

    update

--- Step 13 - Binary Trees/Traversals/InorderTraversal.cpp
+++ Step 13 - Binary Trees/Traversals/InorderTraversal.cpp
@@ -9,19 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+
+// // RECURSIVE
+// class Solution {
+// public:
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> result = {};
+//         inorder(root, result);
+//         return result;
+//     }
+
+//     void inorder(TreeNode* root, vector<int>& result){
+//         if (root == nullptr) return;
+
+//         inorder(root->left, result);
+//         result.push_back(root->val);
+//         inorder(root->right, result);
+//     }
+// };
+
+
+
+// ITERATIVE
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result = {};
-        inorder(root, result);
-        return result;
-    }
+        vector<int> inorder;
+        if (!root) return inorder;
 
-    void inorder(TreeNode* root, vector<int>& result){
-        if (root == nullptr) return;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
 
-        inorder(root->left, result);
-        result.push_back(root->val);
-        inorder(root->right, result);
+        while(true){
+            if (node != nullptr) {
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if (st.empty()) break;
+                node = st.top();
+                inorder.push_back(st.top()->val);
+                st.pop();
+                node = node->right;
+            }
+        }
+        return inorder;
     }
 };

6456d12a35fb6ebfb8e23831c1d9e0b3642c1702
Saksham Aggarwal <saksham28jan@gmail.com>
2026-02-13 23:55:29 +0530

    update

---
+++ Step 13 - Binary Trees/Medium Problems/MaximumPathSum.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int sum = INT_MIN;
+        dfs(root, sum);
+        return sum;
+    }
+
+    int dfs(TreeNode* root, int& sum){
+        if (!root) return 0;
+
+        int left = max(0, dfs(root->left, sum));
+        int right = max(0, dfs(root->right, sum));
+
+        sum = max(sum, left + right + root->val);
+
+        return root->val + max(left, right);
+    }
+};

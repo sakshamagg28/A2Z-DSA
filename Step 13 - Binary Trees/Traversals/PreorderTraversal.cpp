eb0ad9079c2759cf74b01ed720fbaa36d47bb897
Saksham Aggarwal <saksham28jan@gmail.com>
2026-02-13 16:36:50 +0530

    update

--- Step 13 - Binary Trees/Traversals/PreorderTraversal.cpp
+++ Step 13 - Binary Trees/Traversals/PreorderTraversal.cpp
@@ -9,20 +9,49 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// // RECURSIVE
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> result = {};
+//         helper(root, result);
+//         return result;
+//     }
+
+//     void helper(TreeNode* node, vector<int>& result){
+//         if (node == nullptr) return;
+
+//         result.push_back(node->val);
+
+//         helper(node->left, result);
+//         helper(node->right, result);
+//     }
+// };
+
+
+
+// ITERATIVE
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> result = {};
-        helper(root, result);
+        vector<int> result;
+        if (root == nullptr) return result;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+
+            result.push_back(root->val);
+
+            if(root->right) st.push(root->right);
+            if(root->left) st.push(root->left);
+        }
+
         return result;
-    }
-
-    void helper(TreeNode* node, vector<int>& result){
-        if (node == nullptr) return;
-
-        result.push_back(node->val);
-
-        helper(node->left, result);
-        helper(node->right, result);
     }
 };

7c80bd98ece0e2bbab85039f80ebb2df048c4834
Saksham Aggarwal <saksham28jan@gmail.com>
2026-02-13 19:26:15 +0530

    update

--- Step 13 - Binary Trees/Traversals/PostorderTraversal.cpp
+++ Step 13 - Binary Trees/Traversals/PostorderTraversal.cpp
@@ -9,19 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+// // RECURSIVE
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         postorder(root, result);
+//         return result;
+//     }
+    
+//     void postorder(TreeNode* root, vector<int>& result){
+//         if (root == nullptr) return;
+
+//         postorder(root->left, result);
+//         postorder(root->right, result);
+//         result.push_back(root->val);
+//     }
+// };
+
+
+
+// ITERATIVE using 1 stack
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> result;
-        postorder(root, result);
-        return result;
-    }
-    
-    void postorder(TreeNode* root, vector<int>& result){
-        if (root == nullptr) return;
+        vector<int> postorder;
+        if (!root) return postorder;
 
-        postorder(root->left, result);
-        postorder(root->right, result);
-        result.push_back(root->val);
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        TreeNode* lastVisited = nullptr;
+
+        while(node != nullptr || !st.empty()){
+            if (node != nullptr){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if (st.top()->right != nullptr && lastVisited != st.top()->right) {
+                    node = st.top()->right;
+                }
+                else{
+                    postorder.push_back(st.top()->val);
+                    lastVisited = st.top();
+                    st.pop();
+                }
+            }
+        }
+        return postorder;
     }
 };

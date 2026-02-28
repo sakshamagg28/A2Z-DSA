5972466cd017486bcd11ff5b0260aa7ec45c0241
Saksham Aggarwal <saksham28jan@gmail.com>
2026-03-01 01:01:24 +0530

    update

---
+++ Step 11 - Heaps/Medium/KthSmallestElementInAnArray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int kthSmallestElement(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+
+        for (int i = 0; i < k; i++){
+            pq.push(nums[i]);
+        }
+        for (int i = k; i < nums.size(); i++){
+            if (nums[i] < pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};

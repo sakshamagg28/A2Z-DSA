00b11060e4ca9f89ff1f42fccb50fe608549e16d
Saksham Aggarwal <saksham28jan@gmail.com>
2026-03-02 03:46:21 +0530

    update

---
+++ CodeForces/CP-31 Sheet/900/BadBoy.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    for (int k=0; k<t; k++){
+        int n,m,i,j;
+        cin >> n >> m >> i >> j;
+
+        cout << 1 << " " << 1 << " " << n << " " << m << endl;
+    }
+    return 0;
+}

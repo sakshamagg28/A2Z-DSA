4bfb23002d4774f569f0e1b21251a1bb1723b38f
Saksham Aggarwal <saksham28jan@gmail.com>
2026-03-04 03:26:24 +0530

    update

--- CodeForces/CP-31 Sheet/900/Chemistry.cpp
+++ CodeForces/CP-31 Sheet/900/Chemistry.cpp
@@ -1,36 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-
-void isPalindrome(string s, int n, int k){
-    if (n - k == 1){
-        cout << "YES" << endl;
-        return;
-    }
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i] != s[n-i-1]){
-            if (s[i] == s[n-i-2]){
-                s.erase(n-i-1);
-                count++;
-            }
-            // else if (s[i+1] == s[n-i-1]){
-            //     s.erase(i);
-            //     count++;
-            // }
-            else{
-                s.erase(i);
-                s.erase(n-i-1);
-                count += 2;
-            }
-        }
-
-        if (count >= k){
-            cout << "NO" << endl;
-        }
-    }
-
-    if (count >= k) cout << "NO" << endl;
-}
 
 int main(){
     int t;
@@ -40,7 +10,27 @@ int main(){
         cin >> n >> k;
         string s;
         cin >> s;
-        isPalindrome(s, n, k);
+
+        unordered_map<char, int> freq;
+        int extraCount = 0;
+
+        for (char c : s){
+            freq[c]++;
+        }
+        for (auto&p : freq){
+            if (p.second % 2 != 0){
+                extraCount++;
+            }
+        }
+        if (k <= extraCount && extraCount-k <= 1){
+            cout << "YES\n";
+        }
+        else if (k > extraCount){
+            cout << "YES\n";
+        }
+        else{
+            cout << "NO\n";
+        }
     }
     return 0;
 }
---
+++ CodeForces/CP-31 Sheet/900/MakeAP.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t ; i++){
+        long long a,b,c;
+        cin>>a>>b>>c;
+
+        if ((a+c)%2 == 0 && b == (a+c)/2)
+            cout << "YES\n";
+        else if (2*b-c > 0 && (2*b-c)%a == 0)
+            cout << "YES\n";
+        else if ((a+c)%(2*b) == 0)
+            cout << "YES\n";
+        else if (2*b-a > 0 && (2*b-a)%c == 0)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+}

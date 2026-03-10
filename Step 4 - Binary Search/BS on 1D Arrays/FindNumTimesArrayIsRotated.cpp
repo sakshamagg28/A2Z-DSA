37dbceef9997c9eb3fe1acd6f40ec28022f2c587
Saksham Aggarwal <saksham28jan@gmail.com>
2026-03-10 11:27:09 +0530

    update

---
+++ Step 4 - Binary Search/BS on 1D Arrays/FindNumTimesArrayIsRotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findKRotation(vector<int> &nums)  {
+        int low = 0, high = nums.size()-1, mid;
+
+        while (low < high){
+            mid = low + (high-low)/2;
+
+            if (nums[mid] > nums[high]){
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};

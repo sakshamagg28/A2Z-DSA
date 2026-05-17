2d547346f26373af1f0535abba2ad6e3710861f5
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-17 23:17:16 +0530

    update

--- Step 3 - Solve Problems on Arrays/Medium/KadaneAlgorithm.cpp
+++ Step 3 - Solve Problems on Arrays/Medium/KadaneAlgorithm.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int maxSum = INT_MIN;
-        for (int i = 0; i < nums.size(); i++){
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++){
-                sum += nums[j];
+        int sum = 0;
 
-                if (sum > maxSum) maxSum = sum;
-            }
+        for (int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+
+            if (sum > maxSum) maxSum = sum;
+
+            if (sum < 0) sum = 0;
         }
         return maxSum;
     }

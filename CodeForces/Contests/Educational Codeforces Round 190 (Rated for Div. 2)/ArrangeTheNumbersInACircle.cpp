6c94339f7553b7e9be26fab2b6fe573794857e8f
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-18 22:10:32 +0530

    update

---
+++ CodeForces/Contests/Educational Codeforces Round 190 (Rated for Div. 2)/ArrangeTheNumbersInACircle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        ve
+    }
+
+    return 0;
+}

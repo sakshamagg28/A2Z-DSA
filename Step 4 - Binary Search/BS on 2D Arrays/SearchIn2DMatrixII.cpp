833b6ee9a5263cb6bac583c18dd53401de16335d
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-22 03:50:44 +0530

    update

---
+++ Step 4 - Binary Search/BS on 2D Arrays/SearchIn2DMatrixII.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0){
+            if (target == matrix[row][col]){
+                return true;
+            }
+            else if (target < matrix[row][col]){
+                col--;
+            }
+            else row++;
+        }
+        return false;
+    }
+};

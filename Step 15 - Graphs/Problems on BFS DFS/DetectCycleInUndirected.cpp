e30300d125173a00effb521becde3a86842f482c
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-25 03:47:50 +0530

    update

--- Step 15 - Graphs/Problems on BFS DFS/DetectCycleInUndirected.cpp
+++ Step 15 - Graphs/Problems on BFS DFS/DetectCycleInUndirected.cpp
@@ -1,3 +1,5 @@
+// DFS Approach
+
 class Solution{
 public:
     bool dfs(int node, int parent, vector<int> adj[], vector<int>& vis){
@@ -26,3 +28,47 @@ public:
         return false;
     }
 };
+
+
+
+// BFS Approach
+
+// class Solution {
+// public:
+//     bool bfs(int start, vector<int> adj[], vector<int>& vis) {
+//         queue<pair<int, int>> q;   // {node, parent}
+//         vis[start] = 1;
+//         q.push({start, -1});
+
+//         while (!q.empty()) {
+//             auto [node, parent] = q.front();
+//             q.pop();
+
+//             for (int neighbour : adj[node]) {
+//                 if (!vis[neighbour]) {
+//                     vis[neighbour] = 1;
+//                     q.push({neighbour, node});
+//                 }
+//                 else if (neighbour != parent) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+
+//     bool isCycle(int V, vector<int> adj[]) {
+//         vector<int> vis(V, 0);
+
+//         for (int i = 0; i < V; i++) {
+//             if (!vis[i]) {
+//                 if (bfs(i, adj, vis)) {
+//                     return true;
+//                 }
+//             }
+//         }
+
+//         return false;
+//     }
+// };

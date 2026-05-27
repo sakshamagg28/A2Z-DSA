2c2084308233054240940e23a7d95f4228386543
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-28 05:20:00 +0530

    update

---
+++ Step 16 - Dynamic Programming/1D DP/HouseRobberII.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int start, int end){
+        int prev2 = 0;
+        int prev1 = 0;
+        int curr;
+
+        for (int i = start; i <= end; i++){
+            curr = max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        return max (solve(nums, 0, n-2), solve(nums, 1, n-1));
+
+
+    }
+};

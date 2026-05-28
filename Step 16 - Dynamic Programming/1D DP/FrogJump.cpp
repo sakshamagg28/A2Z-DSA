6c1a7d598a8d59521c76b781299694801d994c7d
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-28 06:23:18 +0530

    update

---
+++ Step 16 - Dynamic Programming/1D DP/FrogJump.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int frogJump(vector<int>& heights) {
+        int n = heights.size();
+
+        if (n == 1) return heights[0];
+
+        int prev1 = abs(heights[1] - heights[0]);
+        int prev2 = 0;
+        int curr;
+
+        for (int i = 2; i < n; i++){
+            int oneStep = prev1 + abs(heights[i] - heights[i - 1]);
+
+            int twoStep = prev2 + abs(heights[i] - heights[i - 2]);
+
+            int curr = min(oneStep, twoStep);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        }
+        return curr;
+    }
+};

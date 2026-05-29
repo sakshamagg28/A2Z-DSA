16636e8aa7cd7719ba942ab6571f8e62f898e57a
Saksham Aggarwal <saksham28jan@gmail.com>
2026-05-29 05:57:53 +0530

    update

--- Step 16 - Dynamic Programming/2D-3D DP and DP on Grids/MinimumPathSum.cpp
+++ Step 16 - Dynamic Programming/2D-3D DP and DP on Grids/MinimumPathSum.cpp
@@ -55,3 +55,37 @@ public:
         return dp[n - 1];
     }
 };
+
+
+
+// Space Optimization O(n) - Alternative way to write the code
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> dp(n);
+
+        dp[0] = grid[0][0];
+
+        for (int i=0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (i == 0 && j == 0) continue;
+
+                else if (i > 0 && j > 0){
+                int down = dp[j-1] + grid[i][j];
+                int right = dp[j] + grid[i][j];
+
+                    dp[j] = min(down, right);
+                }
+                else if (i == 0){
+                    dp[j] = dp[j-1] + grid[i][j];
+                }
+                else dp[j] = dp[j] + grid[i][j];
+            }
+        }
+
+        return dp[n-1];
+    }
+};

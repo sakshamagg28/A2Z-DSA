a6fa2daafd8122892679b505bafac7a5413cff01
Saksham Aggarwal <saksham28jan@gmail.com>
2026-06-08 05:42:47 +0530

    update

--- Step 16 - Dynamic Programming/DP on Subsequences/PartitionEqualSubsetSum.cpp
+++ Step 16 - Dynamic Programming/DP on Subsequences/PartitionEqualSubsetSum.cpp
@@ -19,3 +19,33 @@ public:
         return dp[target];
     }
 };
+
+
+
+// Memoization
+class Solution {
+public:
+    bool f(vector<int>& nums, vector<vector<int>>& dp, int target, int index){
+        if (target == 0) return true;
+        if (index == 0) return (nums[0] == target);
+
+        if (dp[index][target] != -1) return dp[index][target];
+
+        bool notTake = f(nums, dp, target, index - 1);
+        bool take = false;
+        if (target >= nums[index]) take = f(nums, dp, target - nums[index], index - 1);
+
+        return dp[index][target] = take || notTake;
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+
+        int target = sum/2;
+        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
+
+        return f(nums, dp, target, nums.size()-1);
+    }
+};
--- Step 16 - Dynamic Programming/DP on Subsequences/SubsetSumEqualsTarget.cpp
+++ Step 16 - Dynamic Programming/DP on Subsequences/SubsetSumEqualsTarget.cpp
@@ -12,4 +12,29 @@ public:
 
         return dp[target];
     }
+};  
+
+
+
+// Tabulation
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int target){
+        int n = arr.size();
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
+
+        for(int i = 0; i < n; i++) dp[i][0] = true;
+        dp[0][arr[0]] = true;
+
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j <= target; j++){
+                bool notTake = dp[i - 1][j];
+                bool take = false;
+                if (arr[i] <= j) take = dp[i-1][j - arr[i]];
+                dp[i][j] = take | notTake;
+            }
+        }
+
+        return dp[n-1][target];
+    }
 };
